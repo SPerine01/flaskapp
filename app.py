e25a9d98fc964eb24576d59ff040bb2fe061074b
Sumayyah Perine <sperine01@gmail.com>
2017-09-21 15:12:30 -0400

    create about page and data for articles

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from data import Articles
+# render_template brings in the the different pages from the template folder
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -9,6 +13,11 @@ def index():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+	return render_template('articles.html', articles = Articles)
+	# passing in the data from data.py
 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'First Article',
+			'body': 'Lorem ipsum',
+			'creator': 'Sumayyah Perine',
+			'create_date': '9-21-2017'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Lorem ipsum',
+			'creator': 'Tupac Shakur',
+			'create_date': '9-21-2017'
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'Lorem ipsum',
+			'creator': 'Sumayyah Perine',
+			'create_date': '9-21-2017'
+		}
+	]
+
+	return articles
